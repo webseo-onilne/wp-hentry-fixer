874f997d7e0d8d7ded54019ef74cfcf52152af94
UncaughtTypeError <nathan.shepherd8@gmail.com>
2017-11-02 11:22:06 +0200

    Update wp-hentry-fixer.php
    Option to remove "hentry" class on "attachment" pages added;
    Option to redirect attachment pages to parent page or home page (if unattached) added;

--- wp-hentry-fixer.php
+++ wp-hentry-fixer.php
@@ -5,11 +5,11 @@ Plugin URI: https://github.com/michaeldoye/wp-hentry-fixer
 Description: Fixes missing hentry errors for single posts and archive pages.
 Author: Web SEO Online (PTY) LTD
 Author URI: https://webseo.co.za
-Version: 0.1.0
+Version: 0.1.1
 
-	Copyright: © 2016 Web SEO Online (PTY) LTD (email : michael@webseo.co.za)
-	License: GNU General Public License v3.0
-	License URI: http://www.gnu.org/licenses/gpl-3.0.html
+  Copyright: © 2016 Web SEO Online (PTY) LTD (email : michael@webseo.co.za)
+  License: GNU General Public License v3.0
+  License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 /**
@@ -48,6 +48,9 @@ function wp_hentry_fixer_page() {
                 <td>
                     <label>
                         <input type="checkbox" name="remove_hentry_class_from_pages" <?php echo esc_attr( get_option('remove_hentry_class_from_pages') ) == 'on' ? 'checked="checked"' : ''; ?> />Yes, remove <code>hentry</code> classes from pages <code>is_page()</code>
+                    </label><br/>
+                    <label>
+                        <input type="checkbox" name="remove_hentry_class_from_attachment_pages" <?php echo esc_attr( get_option('remove_hentry_class_from_attachment_pages') ) == 'on' ? 'checked="checked"' : ''; ?> />Yes, remove <code>hentry</code> classes from attachment pages <code>is_attachment()</code>
                     </label>
                 </td>
             </tr>
@@ -73,7 +76,21 @@ function wp_hentry_fixer_page() {
             </tr>
 
             <tr>
-            	<td><?php submit_button(); ?></td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+
+            <tr>
+                <th align="left" valign="top">Redirect attachment pages to post parent, or home page if no post parent is found</th>
+                <td>
+                    <label>
+                        <input type="checkbox" name="redirect_attachment_pages_to_parent_else_home" <?php echo esc_attr( get_option('redirect_attachment_pages_to_parent_else_home') ) == 'on' ? 'checked="checked"' : ''; ?> />Yes, redirect attachment pages to parent pages or the home page if attachment is <em>unattached</em>
+                    </label>
+                </td>
+            </tr>
+
+            <tr>
+              <td><?php submit_button(); ?></td>
             </tr>
         </table>
 
@@ -86,73 +103,93 @@ function wp_hentry_fixer_page() {
 /**
  * Make sure class doesn't already exist
  */
-	
+  
 if ( ! class_exists( 'HentryFixer' ) ) {
-	
-	/**
-	 * Localisation
-	 **/
-	load_plugin_textdomain( 'HentryFixer', false, dirname( plugin_basename( __FILE__ ) ) . '/' );
+  
+  /**
+   * Localisation
+   **/
+  load_plugin_textdomain( 'HentryFixer', false, dirname( plugin_basename( __FILE__ ) ) . '/' );
 
-	class HentryFixer {
+  class HentryFixer {
 
-	    /**
-	     * constructor
-	     */
-		public function __construct() {
-			add_filter( 'the_content', array( $this, 'hatom_data_in_content'), 100 );	
-			add_filter( 'post_class', array( $this, 'remove_hentry_class'), 100 );	             			
-		}
+      /**
+       * constructor
+       */
+    public function __construct() {
+      add_filter( 'the_content', array( $this, 'hatom_data_in_content'), 100 ); 
+      add_filter( 'post_class', array( $this, 'remove_hentry_class'), 100 );                    
+    }
 
-		/**
-		 * hatom_data_in_content
-		 * Checks post type and injects hatom data.
-		 * @param string $content - html post content
-		 * @return string 
-		 **/
-		public function hatom_data_in_content( $content ) {
+    /**
+     * hatom_data_in_content
+     * Checks post type and injects hatom data.
+     * @param string $content - html post content
+     * @return string 
+     **/
+    public function hatom_data_in_content( $content ) {
 
-			if ( get_option('inject_hatom_on_single_post_pages') == 'on' && is_single() && is_main_query() ) {
-				$content .= $this->get_hatom_data(); 
-			}
-			if ( get_option('inject_hatom_on_archive_pages') == 'on' && is_archive() && is_main_query() ) {
-		        echo $this->get_hatom_data(); 
-			}
-			if ( get_option('inject_hatom_on_posts_page') == 'on' && is_home() && is_main_query() ) {
-				echo $this->get_hatom_data();
-			}
-			return $content;
-		}
-		 
-		/**
-		 * get_hatom_data
-		 * Contstructs hatom/hentry markup for post content
-		 * @return string 
-		 **/
-		private function get_hatom_data() {
-		    $html  = '<div class="hatom-extra" style="display:none;visibility:hidden;">';
-		    $html .= '<span class="entry-title">'.get_the_title().'</span>';
-		    $html .= '<span class="updated"> '.get_the_modified_time('F jS, Y').'</span>';
-		    $html .= '<span class="author vcard"><span class="fn">'.get_the_author().'</span></span></div>';
-		    return $html;
-		}
-		 
-		/**
-		 * remove_hentry_class
-		 * Removes "hentry" class from body tag of page
-		 * @param string $classes - body tag classes
-		 * @return string 
-		 **/
-		public function remove_hentry_class( $classes ) {
+      if ( get_option('inject_hatom_on_single_post_pages') == 'on' && is_single() && is_main_query() ) {
+        $content .= $this->get_hatom_data(); 
+      }
+      if ( get_option('inject_hatom_on_archive_pages') == 'on' && is_archive() && is_main_query() ) {
+            echo $this->get_hatom_data(); 
+      }
+      if ( get_option('inject_hatom_on_posts_page') == 'on' && is_home() && is_main_query() ) {
+        echo $this->get_hatom_data();
+      }
+      return $content;
+    }
+     
+    /**
+     * get_hatom_data
+     * Contstructs hatom/hentry markup for post content
+     * @return string 
+     **/
+    private function get_hatom_data() {
+        $html  = '<div class="hatom-extra" style="display:none;visibility:hidden;">';
+        $html .= '<span class="entry-title">'.get_the_title().'</span>';
+        $html .= '<span class="updated"> '.get_the_modified_time('F jS, Y').'</span>';
+        $html .= '<span class="author vcard"><span class="fn">'.get_the_author().'</span></span></div>';
+        return $html;
+    }
+     
+    /**
+     * remove_hentry_class
+     * Removes "hentry" class from body tag of page
+     * @param string $classes - body tag classes
+     * @return string 
+     **/
+    public function remove_hentry_class( $classes ) {
 
-			if ( get_option('remove_hentry_class_from_pages') == 'on' && is_page() && is_main_query() ) {
-				$classes = array_diff( $classes, array( 'hentry' ) );
-			}
-			return $classes;
-		}
+      if ( get_option('remove_hentry_class_from_pages') == 'on' && is_page() && is_main_query() ) {
+        $classes = array_diff( $classes, array( 'hentry' ) );
+      }
+      if ( get_option('remove_hentry_class_from_attachment_pages') == 'on' && is_attachment() && is_main_query() ) {
+        $classes = array_diff( $classes, array( 'hentry' ) );
+      }
+      return $classes;
+    }
+     
+    /**
+     * redirect_attachment_pages
+     * Redirect attachment pages to post parent, or home page if no post parent is found
+     **/
+    public function redirect_attachment_pages() {
+      if ( get_option('redirect_attachment_pages_to_parent_else_home') == 'on' && is_attachment() ) {
+        global $post;
+        if ( $post && $post->post_parent ) {
+          wp_redirect( get_permalink( $post->post_parent ), 301 );
+          exit;
+        } else {
+          wp_redirect( home_url( '/' ), 301 );
+          exit;
+        }
+      }
+    }
 
-	}
-	
-	// finally instantiate our plugin class and add it to the set of globals
-	$GLOBALS['HentryFixer'] = new HentryFixer();
+  }
+  
+  // finally instantiate our plugin class and add it to the set of globals
+  $GLOBALS['HentryFixer'] = new HentryFixer();
 }
